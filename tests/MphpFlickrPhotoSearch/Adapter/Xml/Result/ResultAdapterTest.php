42bbff003284d234a1fdb847f69f4e428e702065
David White <git@monkeyphp.com>
2013-03-19 21:55:18 +0000

    Added ResultAdapterTest.php

---
+++ tests/MphpFlickrPhotoSearch/Adapter/Xml/Result/ResultAdapterTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * ResultAdapterTest.php
+ *
+ * PHP Version PHP 5.3.10
+ *
+ * @category    MphpFlickrPhotoSearchTest
+ * @package     MphpFlickrPhotoSearchTest
+ * @subpackage  MphpFlickrPhotoSearchTest\Adapter\Xml\Result
+ * @author      David White [monkeyphp] <git@monkeyphp.com>
+ */
+namespace MphpFlickrPhotoSearchTest\Adapter\Xml\Result;
+
+/**
+ * ResultAdapterTest
+ *
+ * @category    MphpFlickrPhotoSearchTest
+ * @package     MphpFlickrPhotoSearchTest
+ * @subpackage  MphpFlickrPhotoSearchTest\Adapter\Xml\Result
+ * @author      David White [monkeyphp] <git@monkeyphp.com>
+ */
+class ResultAdapterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that we can get the accuracy value
+     */
+    public function testGetAccuracy()
+    {
+        $this->markTestIncomplete();
+    }
+    
+}
