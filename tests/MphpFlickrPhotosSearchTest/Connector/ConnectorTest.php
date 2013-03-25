0b62dffe94916fb35b230f0f7bf0fc8d787ae514
David White <git@monkeyphp.com>
2013-03-25 22:00:02 +0000

    Added ConnectorTest.php

---
+++ tests/MphpFlickrPhotosSearchTest/Connector/ConnectorTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * ConnectorTest.php
+ *
+ * PHP Version PHP 5.3.10
+ *
+ * @category    MphpFlickrPhotosSearchTest
+ * @package     MphpFlickrPhotosSearchTest
+ * @subpackage  MphpFlickrPhotosSearchTest\Connector
+ * @author      David White [monkeyphp] <git@monkeyphp.com>
+ */
+namespace MphpFlickrPhotosSearchTest\Connector;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * ConnectorTest
+ *
+ * @category    MphpFlickrPhotosSearchTest
+ * @package     MphpFlickrPhotosSearchTest
+ * @subpackage  MphpFlickrPhotosSearchTest\Connector
+ * @author      David White [monkeyphp] <git@monkeyphp.com>
+ */
+class ConnectorTest extends PHPUnit_Framework_TestCase
+{
+
+    public function test__construct()
+    {
+        $apiKey = '0123456789';
+        $connector = new \MphpFlickrPhotosSearch\Connector\Connector($apiKey);
+
+        $parameters = array('method' => 'flickr.photos.search', 'tags' => 'metallica');
+
+        $reflectionObject = new \ReflectionObject($connector);
+        $reflectionMethod = $reflectionObject->getMethod('prepareParameters');
+        $reflectionMethod->setAccessible(true);
+
+        $this->assertSame($parameters, $reflectionMethod->invoke($connector, $parameters));
+    }
+
+}
